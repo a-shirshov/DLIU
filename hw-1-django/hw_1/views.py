7bcdd022740a10bb7f87c6ad2cc1b31316b54abe
a-shirshov <artyomsh01@yandex.ru>
2024-04-03 22:35:23 +0300

    dz-update

--- hw-1-django/hw_1/views.py
+++ hw-1-django/hw_1/views.py
@@ -6,15 +6,15 @@ from PIL import Image
 
 imageClassList = {'0': 'Бледная поганка', '1': 'Мухомор', '2': 'Подосиновик'}  #Сюда указать классы
 imageCriteriaList = {'0': 'Белый цвет, наличие выраженной вольвы', '1': 'Красноватый цвет, толстомясистая шляпка, наличие вкрапинок', '2': 'Наличие оранжево-коричневого цвета, ножка не совсем белая'}
-imageDescription = {'0': 'Подосиновик - съедобный гриб с крупной коричневой шляпкой, короткой толстой ножкой и белым трубчатым слоем под шляпкой. Часто встречается в лиственных лесах.',
+imageDescription = {'2': 'Подосиновик - съедобный гриб с крупной коричневой шляпкой, короткой толстой ножкой и белым трубчатым слоем под шляпкой. Часто встречается в лиственных лесах.',
                     '1': 'Мухомор - яркий гриб с красной шляпкой, усеянной белыми пятнами, и белой ножкой с кольцом. Часто встречается в хвойных и лиственных лесах.',
-                    '2': 'Бледная поганка - гриб с белой или серовато-белой шляпкой, ножкой без кольца и пластинчатым слоем под шляпкой. Он часто встречается в лесах и на лугах.'}
-imageTasty = {'0': 'Подосиновики считаются съедобными и широко используются в кулинарии.',
+                    '0': 'Бледная поганка - гриб с белой или серовато-белой шляпкой, ножкой без кольца и пластинчатым слоем под шляпкой. Он часто встречается в лесах и на лугах.'}
+imageTasty = {'2': 'Подосиновики считаются съедобными и широко используются в кулинарии.',
                     '1': 'Многие виды мухоморов считаются ядовитыми и могут вызывать серьезное отравление. Не рекомендуется употреблять без должной экспертизы.',
-                    '2': 'Бледная поганка является ядовитым грибом и не подходит для употребления в пищу.'}
-imageLatin = {'0': 'Leccinum spp',
+                    '0': 'Бледная поганка является ядовитым грибом и не подходит для употребления в пищу.'}
+imageLatin = {'2': 'Leccinum spp',
                     '1': 'Amanita spp',
-                    '2': 'Chlorophyllum spp'}
+                    '0': 'Chlorophyllum spp'}
 
 def scoreImagePage(request):
     return render(request, 'scorepage.html')
@@ -32,7 +32,7 @@ def predictImage(request):
 def predictImageData(modelName, filePath):
     img = Image.open(filePath).convert("RGB")
     img = np.asarray(img.resize((32, 32), Image.ANTIALIAS))
-    sess = onnxruntime.InferenceSession(r'/home/artyom/VUZ_Learning/DLIU/hw-1-django/media/models/mushrooms_CNN_MobileNet.onnx') #<-Здесь требуется указать свой путь к модели
+    sess = onnxruntime.InferenceSession(r'/home/artyom/DLIU/hw-1-django/media/models/mushrooms_CNN_MobileNet.onnx') #<-Здесь требуется указать свой путь к модели
     outputOFModel = np.argmax(sess.run(None, {'input': np.asarray([img]).astype(np.float32)}))
     score = imageClassList[str(outputOFModel)]
     criteria =  imageCriteriaList[str(outputOFModel)]
